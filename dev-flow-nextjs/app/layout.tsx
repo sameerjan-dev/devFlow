3c38084c2bf58bcd00049bbbb474c1f1b5bcf3ea
sameerjan1 <136233911+sameerjan1@users.noreply.github.com>
2025-01-24 11:34:58 +0500

    setup next-themes to apply themes according to light and dark mode of your system

--- dev-flow-nextjs/app/layout.tsx
+++ dev-flow-nextjs/app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import { Geist, Geist_Mono } from "next/font/google";
 import { ReactNode } from "react";
 import "./globals.css";
+import ThemeProvider from "@/context/Theme";
 
 const inter = localFont({
   src: "./fonts/InterVF.ttf",
@@ -30,11 +31,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
